56f49386f093429c8e38783ca2af7bb1dac78e5f
Andy Fragen <andy@thefragens.com>
2012-12-28 17:03:23 -0800

    realign tabs in css

--- views/legend.css.php
+++ views/legend.css.php
@@ -1,14 +1,14 @@
-#legend_box {
-	font: bold 10px/4em sans-serif; text-align: center;
-}
+	#legend_box {
+		font: bold 10px/4em sans-serif; text-align: center;
+	}
 
-#legend a {
-	text-decoration:none;
-}
+	#legend a {
+		text-decoration:none;
+	}
 
-#legend li {
-	display: inline;
-	list-style-type:none;
-	padding: 7px;
-	margin-left: 0.7em;
-}
+	#legend li {
+		display: inline;
+		list-style-type:none;
+		padding: 7px;
+		margin-left: 0.7em;
+	}
