cc54a36c1996b8a36315f780c0e661e1a46ec6e9
Andy Fragen <andy@thefragens.com>
2012-12-29 10:38:25 -0800

    fix for legend css, will merge and move to legend.css.php

--- views/category.css.php
+++ views/category.css.php
@@ -1,29 +1,31 @@
 
 <!-- The Events Calendar Category Colors <?php echo TribeEventsCategoryColors::VERSION ?> generated CSS -->
 <style type="text/css" media="screen">
-.tribe-events-calendar a {
-	font-weight: <?php echo $options['font_weight']  ?>;
-}
+	#legend_box #legend li span { cursor: pointer; }
 
-<?php for ($i = 0; $i < $teccc->count; $i++): ?>
+	.tribe-events-calendar a {
+		font-weight: <?php echo $options['font_weight']  ?>;
+	}
 
-.tribe-events-calendar .cat_<?php echo($teccc->slugs[$i]) ?> a {
-	color: <?php echo $options[$teccc->slugs[$i].'-text'] ?>;
-}
+	<?php for ($i = 0; $i < $teccc->count; $i++): ?>
 
-.cat_<?php echo $teccc->slugs[$i] ?>,
-.tribe-events-calendar .cat_<?php echo $teccc->slugs[$i] ?>,
-.cat_<?php echo $teccc->slugs[$i] ?>,
-.tribe-events-tooltip .tribe-events-event-title {
-	background-color: <?php echo $options[$teccc->slugs[$i].'-background'] ?>;
-	border-left: 5px solid <?php echo $options[$teccc->slugs[$i].'-border'] ?>;
-	border-right: 5px solid transparent;
-	color: <?php echo $options[$teccc->slugs[$i].'-text'] ?>;
-}
-<?php endfor ?>
+	.tribe-events-calendar .cat_<?php echo($teccc->slugs[$i]) ?> a {
+		color: <?php echo $options[$teccc->slugs[$i].'-text'] ?>;
+	}
 
-<?php if (isset($options['add_legend']) and !isset($options['custom_legend_css'])): ?>
-<?php $teccc->view('legend.css') ?>
-<?php endif ?>
+	.cat_<?php echo $teccc->slugs[$i] ?>,
+	.tribe-events-calendar .cat_<?php echo $teccc->slugs[$i] ?>,
+	.cat_<?php echo $teccc->slugs[$i] ?>,
+	.tribe-events-tooltip .tribe-events-event-title {
+		background-color: <?php echo $options[$teccc->slugs[$i].'-background'] ?>;
+		border-left: 5px solid <?php echo $options[$teccc->slugs[$i].'-border'] ?>;
+		border-right: 5px solid transparent;
+		color: <?php echo $options[$teccc->slugs[$i].'-text'] ?>;
+	}
+	<?php endfor ?>
+
+	<?php if (isset($options['add_legend']) and !isset($options['custom_legend_css'])): ?>
+		<?php $teccc->view('legend.css') ?>
+	<?php endif ?>
 
 </style>
