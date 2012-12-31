b03869a10a6fe6b04fa4d263f68d85b28f29b165
Andy Fragen <andy@thefragens.com>
2012-12-31 08:11:48 -0800

    code cleanup

--- resources/jquery-miniColors-init.js
+++ resources/jquery-miniColors-init.js
@@ -1,11 +1,11 @@
-jQuery(document).ready( function() {
+jQuery(document).ready(function($) {
 
 	var consoleTimeout;
 
 	// Display the results of the change callback on any minicolors input
-	jQuery('INPUT[type=minicolors]').on('change', function() {
+	$('INPUT[type=minicolors]').on('change', function() {
 
-		var input = jQuery(this),
+		var input = $(this),
 			hex = input.val(),
 			opacity = input.attr('data-opacity'),
 			text;
@@ -13,13 +13,13 @@ jQuery(document).ready( function() {
 		// Generate text to show in console
 		text = hex ? hex : 'transparent';
 		if( opacity ) text += ', ' + opacity;
-		text += ' / ' + jQuery.minicolors.rgbString(input)
+		text += ' / ' + $.minicolors.rgbString(input)
 
 		// Show text in console; disappear after a few seconds
-		jQuery('#console').text(text).addClass('busy');
+		$('#console').text(text).addClass('busy');
 		clearTimeout(consoleTimeout);
 		consoleTimeout = setTimeout( function() {
-			jQuery('#console').removeClass('busy');
+			$('#console').removeClass('busy');
 		}, 3000);
 
 	});
