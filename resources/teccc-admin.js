879984f0076f89bc04ea6daa77e6abc4ad9a9f6b
WebBaker <barry@freshlybakedwebsites.net>
2012-12-31 07:41:33 -0800

    Brushed up the show/hide for a less abrupt effect (fades out before the whitespace removed/adds the whitespace then fades in)

--- resources/teccc-admin.js
+++ resources/teccc-admin.js
@@ -3,8 +3,7 @@
  */
 jQuery(document).ready(function($) {
 	var legendOption = $("#category_legend_setting").find("input");
-	var customCSSOption = $("#legend_custom_css");
-	var superpowersOption = $("#legend_superpowers_setting");
+	var relatedOptions = $("tr.legend_related");
 
 
 	/**
@@ -12,14 +11,10 @@ jQuery(document).ready(function($) {
 	 * option is displayed (or else it is hidden).
 	 */
 	function toggleSuperpowersVisibility() {
-		if ($(legendOption).attr("checked") === "checked") {
-			$(customCSSOption).show();
-			$(superpowersOption).show();
-		}
-		else {
-			$(customCSSOption).hide();
-			$(superpowersOption).hide();
-		}
+		if ($(legendOption).attr("checked") === "checked")
+			$(relatedOptions).fadeTo(500, 1, function() { $(relatedOptions).show(); });
+		else
+			$(relatedOptions).fadeTo(500, 0, function () { $(relatedOptions).hide(); });
 	}
 
 	// Toggle Legend Superpowers visibility initially, after the page loads
--- views/optionsform.php
+++ views/optionsform.php
@@ -71,7 +71,7 @@
 		</td>
 	</tr>
 	
-	<tr id="legend_superpowers_setting">
+	<tr class="legend_related">
 		<th scope="row"><!-- Add Legend Superpowers --></th>
 		<td colspan="5" style="padding-top:0;">
 			<label> <input name="teccc_options[legend_superpowers]" type="checkbox" value="1" <?php echo checked('1', $options['legend_superpowers'], false) ?> /> Check to add Legend Superpowers. </label>
@@ -79,8 +79,8 @@
 		</td>
 	</tr>
 
-	<tr id="legend_custom_css">
-		<th scope="row">Custom Legend CSS</th>
+	<tr class="legend_related">
+		<th scope="row"><label>Custom Legend CSS</label></th>
 		<td colspan="5">
 			<label> <input name="teccc_options[custom_legend_css]" type="checkbox" value="1" <?php echo checked('1', $options['custom_legend_css'], false) ?> /> Check to use your own CSS for category legend. </label>
 		</td>
