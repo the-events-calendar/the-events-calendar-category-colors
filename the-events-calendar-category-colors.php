5bda405a931895b9b8e7a36b60c459be462fae20
Andy Fragen <andy@thefragens.com>
2013-01-03 18:29:00 -0800

    remove extra text from readme

--- the-events-calendar-category-colors.php
+++ the-events-calendar-category-colors.php
@@ -10,35 +10,6 @@
  * License: GNU General Public License v2
  * License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  *
- * The Events Calendar Category Colors
- *
- * This plugin adds background coloring to The Events Calendar plugin.
- *
- * @package      the-events-calendar-pro-alarm
- * @link         https://github.com/afragen/events-calendar-category-colors/
- * @link         http://wordpress.org/extend/plugins/the-events-calendar-category-colors/
- * @author       Andy Fragen <andy@thefragens.com>
- * @copyright    Copyright (c) 2012, Andrew Fragen
- *
- * The Events Calendar Category Colors is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License version 2, as published by the
- * Free Software Foundation.
- *
- * You may NOT assume that you can use any other version of the GPL.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details
- *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to:
- *
- *      Free Software Foundation, Inc.
- *      51 Franklin St, Fifth Floor
- *      Boston, MA  02110-1301  USA
- *
- * The license for this software can also likely be found here:
- * http://www.gnu.org/licenses/gpl-2.0.html*
  */
 
 
