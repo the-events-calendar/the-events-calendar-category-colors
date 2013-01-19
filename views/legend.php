f1e38c72500ec4b02ba4ae26c28b4a91c08aaf74
Andy Fragen <andy@thefragens.com>
2013-01-19 13:49:57 -0800

    possible fix for 'category' translation in link

--- views/legend.php
+++ views/legend.php
@@ -5,11 +5,11 @@
 		<?php for ($i = 0; $i < $teccc->count; $i++): ?>
 
 			<li class="cat_<?php esc_attr_e($teccc->slugs[$i]) ?>">
-				<a href="<?php esc_attr_e($tec->getLink().'category/'.$teccc->slugs[$i]) ?>">
+				<a href="<?php esc_attr_e($tec->getLink().trailingslashit(sanitize_title(__( 'category', 'tribe-events-calendar' ))).$teccc->slugs[$i]) ?>">
 					<?php esc_html_e($teccc->names[$i]) ?>
 				</a>
 				<?php if ($legendData): ?>
-					<input type="hidden" name="teccc-slug[<?php echo $i ?>]" value="<?php esc_attr_e($teccc->slugs[$i]) ?>" />
+					<input type="hidden" name="teccc->slugs[<?php echo $i ?>]" value="<?php esc_attr_e($teccc->slugs[$i]) ?>" />
 				<?php endif ?>
 			</li>
 
