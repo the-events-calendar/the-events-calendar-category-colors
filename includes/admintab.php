2a2cb202af7eaf051b1c5ee585eaf0db98c64d56
Andy Fragen <andy@thefragens.com>
2013-01-26 16:46:23 -0800

    3.0 instructions for removing/adding legend

--- includes/admintab.php
+++ includes/admintab.php
@@ -16,10 +16,10 @@ return array(
 			'type' => 'html',
 			'html' => '<p>The Events Calendar Category Colors plugin was inspired by the tutorial <a href="http://tri.be/coloring-your-category-events/">Coloring Your Category Events</a>.</p>'
 		),
-// 		'legend' => array(
-// 			'type' => 'html',
-// 			'html' => '<p>To include a Category Color legend above your calendar you will need to place a copy of ecp-page-template.php in your theme\'s <strong>events</strong> directory, similar to <code>my-theme/events/ecp-page-template.php</code>. This file is found in The Events Calendar plugin\'s <strong>views</strong> directory. Please refer to <a href="http://tri.be/themers-guide-to-the-events-calendar/">Themer\'s Guide for The Events Calendar</a> for reference.</p><p>Within your copy of ecp-page-template.php you will need to insert <code>&lt;?php teccc_legend_hook(); ?&gt;</code> where you want the legend to appear.'
-// 		),
+ 		'legend' => array(
+ 			'type' => 'html',
+ 			'html' => '<p>The Category Colors legend is inserted using the new <i>tribe_events_calendar_before_the_grid</i> filter. To remove it from this default location and add it back using any of the new filters, add the following lines to your theme\'s functions.php file.</p><p><code>add_filter(\'tribe_events_calendar_before_the_grid\', \'\', 99);</code><br /><code>add_filter(\'tribe_events_calendar_filter_of_choice\', \'TribeEventsCategoryColorsPublic::show_legend\');</code></p><p>Obviously you will need to replace <i>tribe_events_calendar_filter_of_choice</i> with a real filter. Please refer to new TEC filter guide for  options.</p>'
+ 		),
 		'info-end' => array(
 			'type' => 'html',
 			'html' => '</div>'
