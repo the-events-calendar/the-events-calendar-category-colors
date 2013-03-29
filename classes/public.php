d70eb6518f799be789cf3e970e2211162f7ac1d2
Andy Fragen <andy@thefragens.com>
2013-03-28 18:10:32 -0700

    array check for multiple eventDisplay views

--- classes/public.php
+++ classes/public.php
@@ -17,8 +17,9 @@ class TribeEventsCategoryColorsPublic {
 
 
 	public function add_colored_categories($query) {
+		$views = array('month', 'upcoming', 'day');
 		if (isset($query->query_vars['post_type']) and $query->query_vars['post_type'] == 'tribe_events')
-			if (isset($query->query_vars['eventDisplay']) and $query->query_vars['eventDisplay'] == 'month')
+			if (isset($query->query_vars['eventDisplay']) and in_array($query->query_vars['post_type'], $views))
 				$this->add_effects();
 	}
 
