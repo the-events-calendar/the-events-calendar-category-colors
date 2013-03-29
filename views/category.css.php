46d44483ed85c4a37d11b6882faf98b4094a5d51
Andy Fragen <andy@thefragens.com>
2013-03-28 18:09:54 -0700

    fixes for CSS list and day views

--- views/category.css.php
+++ views/category.css.php
@@ -4,6 +4,8 @@
 	.tribe-events-calendar a {
 		font-weight: <?php echo $options['font_weight']  ?>;
 	}
+	
+	.tribe-events-list .vevent.hentry h2 { padding-left: 5px; }
 
 	<?php for ($i = 0; $i < $teccc->count; $i++): ?>
 
@@ -19,8 +21,10 @@
 		border-left: 5px solid <?php echo $options[$teccc->slugs[$i].'-border'] ?>;
 		border-right: 5px solid transparent;
 		color: <?php echo $options[$teccc->slugs[$i].'-text'] ?>;
+		padding-left: 5px;
 	}
-	
+
+/*  CSS for IDs  */
 	.tribe-events-calendar .tribe-events-category-<?php echo($teccc->IDs[$i]) ?> a,
 	.tribe-events-category-<?php echo($teccc->IDs[$i]) ?> h2.entry-title a {
 		color: <?php echo $options[$teccc->slugs[$i].'-text'] ?>;
@@ -33,8 +37,12 @@
 		border-left: 5px solid <?php echo $options[$teccc->slugs[$i].'-border'] ?>;
 		border-right: 5px solid transparent;
 		color: <?php echo $options[$teccc->slugs[$i].'-text'] ?>;
+		padding-left: 5px;
 	}
 
+	
+/*  CSS for IDs end  */
+	
 	<?php endfor ?>
 
 	<?php if (isset($options['add_legend']) and !isset($options['custom_legend_css'])): ?>
