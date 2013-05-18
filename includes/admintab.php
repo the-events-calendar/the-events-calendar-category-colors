595fb99d6a972012b0841f208083e86db886af84
Andy Fragen <andy@thefragens.com>
2013-05-18 12:39:05 -0700

    instructions in settings tab

--- includes/admintab.php
+++ includes/admintab.php
@@ -18,7 +18,7 @@ return array(
 		),
  		'legend' => array(
  			'type' => 'html',
- 			'html' => '<p>The Category Colors legend is inserted using the new <i>tribe_events_calendar_before_the_grid</i> filter. To remove it from this default location and add it back using any of the new filters, add the following line to your theme\'s functions.php file.</p><p><code>teccc_reposition_legend(\'tribe_events_calendar_filter_of_choice\');</code></p><p>Obviously you will need to replace <i>tribe_events_calendar_filter_of_choice</i> with a real filter. Please refer to new TEC filter guide for options.</p>'
+ 			'html' => '<p>The Category Colors legend is inserted using the new <i>tribe_events_calendar_before_the_grid</i> filter. To remove it from this default location and add it back using any of the new filters, add the following line to your theme\'s functions.php file.</p><p><code>teccc_reposition_legend( \'tribe_events_calendar_filter_of_choice\' );</code></p><p>Obviously you will need to replace <code>tribe_events_calendar_filter_of_choice</code> with a real filter. Please refer to new TEC filter guide for options.</p><p>If you want to make minor additions to the default Category Colors legend CSS you may add them by using the action hook <code>teccc_add_legend_css</code> in your theme\'s functions.php file.</p><p><code>add_action( \'teccc_add_legend_css\', \'my_extra_legend_css\' );</code></p>'
  		),
 		'info-end' => array(
 			'type' => 'html',
