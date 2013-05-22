27c4d1d4ab189e52dacf974e27383659d7939e8d
barryhughes <barry@codingkills.me>
2013-05-22 11:41:58 -0700

    Fixes #10 superpowers should now work and keep working after navigation-by-ajax

--- views/legend.php
+++ views/legend.php
@@ -8,9 +8,7 @@
 				<a href="<?php esc_attr_e($tec->getLink().trailingslashit(sanitize_title(__( 'category', 'tribe-events-calendar' ))).$teccc->slugs[$i]) ?>">
 					<?php esc_html_e($teccc->names[$i]) ?>
 				</a>
-				<?php if (isset($legendData) and $legendData): ?>
-					<input type="hidden" value="<?php esc_attr_e($teccc->slugs[$i]) ?>" />
-				<?php endif ?>
+				<input type="hidden" value="<?php esc_attr_e($teccc->slugs[$i]) ?>" />
 			</li>
 
 		<?php endfor ?>
