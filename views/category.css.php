093d67adef1f949e3e3f0b750f621d1ab3ec6414
Andy Fragen <andy@thefragens.com>
2013-08-04 20:41:52 -0700

    CSS and add_filter to colorize events following mini-calendar in TECPro

--- views/category.css.php
+++ views/category.css.php
@@ -11,7 +11,9 @@
 			$slug = esc_attr( $attributes[Tribe_Events_Category_Colors::SLUG] );
 			$name = esc_attr( $attributes[Tribe_Events_Category_Colors::NAME] );
 		?>
-	.tribe-events-calendar .tribe-events-category-<?php echo $slug ?> a {
+	.tribe-events-calendar .tribe-events-category-<?php echo $slug ?> a,
+	.tribe-mini-calendar-event h2.tribe-events-category-<?php echo $slug ?> a,
+	.tribe-mini-calendar-event h2.tribe-events-category-<?php echo $slug ?> a:hover {
 		color: <?php echo $options[ $slug.'-text' ] ?>;
 		text-decoration: none;
 	}
@@ -20,7 +22,7 @@
 	.tribe-events-calendar .tribe-events-category-<?php echo $slug ?>,
 	#tribe-events-content .tribe-events-category-<?php echo $slug ?> > .tribe-events-tooltip h4.summary,
 	.tribe-events-category-<?php echo $slug ?> > .tribe-events-tooltip h4.summary,
-	table.tribe-mini-calendar td.tribe-events-category-<?php echo $slug ?> {
+	.tribe-mini-calendar-event h2.tribe-events-category-<?php echo $slug ?> a {
 		background-color: <?php echo $options[ $slug.'-background' ] ?>;
 		border-left: 5px solid <?php echo $options[ $slug.'-border' ] ?>;
 		border-right: 5px solid transparent;
@@ -28,7 +30,8 @@
 		padding-left: 5px;
 	}
 
-	.tribe-events-category-<?php echo $slug ?> h2.tribe-events-list-event-title a {
+	.tribe-events-category-<?php echo $slug ?> h2.tribe-events-list-event-title a,
+	.tribe-mini-calendar-event h2.tribe-events-category-<?php echo $slug ?> a {
 		width: 93%;
 		display: block;
 	}
