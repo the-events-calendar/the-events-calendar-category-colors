8b4a5376cd9727da7d339c14554587f0408634bb
Andy Fragen <andy@thefragens.com>
2013-08-07 16:38:24 -0700

    simplified widget colorizing, found better CSS selector

--- classes/class-widgets.php
+++ classes/class-widgets.php
@@ -1,26 +1,12 @@
 <?php
 class Tribe_Events_Category_Colors_Widgets {
-	protected $teccc = null;
-
-	public function __construct() {
-	
-	}
-	
+	protected $teccc = null;	
 
 	public function add_widget_link_css( $slug ) {
 		$css = array();
-		if( class_exists( 'TribeEventsPro') ) {
-			$css[] = '.tribe-mini-calendar-event h2.tribe-events-category-' .  $slug .' a,';
-			$css[] = '.tribe-mini-calendar-event h2.tribe-events-category-' .  $slug . ' a:hover,';
-			$css[] = '.tribe-mini-calendar-event h2.tribe-events-category-' .  $slug . ' a:visited,';
-			$css[] = '.tribe-events-adv-list-widget h4.tribe-events-category-' .  $slug . ' a,';
-			$css[] = '.tribe-events-adv-list-widget h4.tribe-events-category-' .  $slug . ' a:hover,';
-			$css[] = '.tribe-events-adv-list-widget h4.tribe-events-category-' .  $slug . ' a:visited,';
-		} else {
-			$css[] = '.tribe-events-list-widget h4.tribe-events-category-' .  $slug . ' a,';
-			$css[] = '.tribe-events-list-widget h4.tribe-events-category-' .  $slug . ' a:hover,';
-			$css[] = '.tribe-events-list-widget h4.tribe-events-category-' .  $slug . ' a:visited,';
-		}
+		$css[] = '.widget-area .widget .tribe-events-category-' .  $slug . ' a,';
+		$css[] = '.widget-area .widget .tribe-events-category-' .  $slug . ' a:hover,';
+		$css[] = '.widget-area .widget .tribe-events-category-' .  $slug . ' a:visited,';		
 		$css[] = '';
 		$css = implode( "\n", $css );
 		echo $css;
@@ -29,29 +15,18 @@ class Tribe_Events_Category_Colors_Widgets {
 	
 	public function add_widget_background_css( $slug ) {
 		$css = array();
-		if( class_exists( 'TribeEventsPro' ) ) {
-			$css[] = '.tribe-mini-calendar-event h2.tribe-events-category-' .  $slug . ' a,';
-			$css[] = '.tribe-events-adv-list-widget h4.tribe-events-category-' .  $slug . ' a,';
-		} else {
-			$css[] = '.tribe-events-list-widget h4.tribe-events-category-' .  $slug . ' a,';
-		}
-			$css[] = '';
+		$css[] = '.widget-area .widget .tribe-events-category-' .  $slug . ' a,';
+		$css[] = '';
 		$css = implode( "\n", $css );
 		echo $css;
 	}
 	
 	public function add_widget_display_css( $slug ) {
 		$css = array();
-		if( class_exists( 'TribeEventsPro' ) ) {
-			$css[] = '.tribe-mini-calendar-event h2.tribe-events-category-' .  $slug . ' a,';
-			$css[] = '.tribe-events-adv-list-widget h4.tribe-events-category-' .  $slug . ' a,';
-		} else {
-			$css[] = '.tribe-events-list-widget h4.tribe-events-category-' .  $slug . ' a,';
-		}
-			$css[] = '';
+		$css[] = '.widget-area .widget .tribe-events-category-' .  $slug . ' a,';
+		$css[] = '';
 		$css = implode( "\n", $css );
 		echo $css;
-
 	}
 	
 }
