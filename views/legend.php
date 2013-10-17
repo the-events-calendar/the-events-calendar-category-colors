7cd7b5789598dd94ec73034e05cb281ddbbc3d60
Andy Fragen <andy@thefragens.com>
2013-10-17 13:16:11 -0700

    change text domain to be more distinct

--- views/legend.php
+++ views/legend.php
@@ -8,7 +8,7 @@
 				$name = esc_attr($attributes[Tribe_Events_Category_Colors::NAME]);
 			?>
 			<li class="tribe-events-category-<?php echo $slug ?> tribe-events-category-<?php esc_attr_e($id) ?>">
-				<a href="<?php esc_attr_e( $tec->getLink().trailingslashit( sanitize_title( __( 'category', 'teccc' ) ) ) .$slug ) ?>">
+				<a href="<?php esc_attr_e( $tec->getLink().trailingslashit( sanitize_title( __( 'category', 'events-calendar-category-colors' ) ) ) .$slug ) ?>">
 					<?php echo $name ?>
 				</a>
 				<input type="hidden" value="<?php echo $slug ?>" />
