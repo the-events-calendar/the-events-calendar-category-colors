28bc73951d92f0a571f50acebd166c5b2a7e096a
Andy Fragen <andy@thefragens.com>
2013-10-17 12:29:01 -0700

    iris javascript init

--- resources/wp-color-picker-init.js
+++ resources/wp-color-picker-init.js
@@ -14,14 +14,6 @@ jQuery(document).ready(function($){
 		// show a group of common colors beneath the square
 		// or, supply an array of colors to customize further
 		palettes: false,
-	};
- 
-	$('.teccc-color-picker').wpColorPicker(myOptions);
-
-	$('.teccc-color-picker').wpColorPicker();
-	
-	$('.teccc-color-picker').iris({
-		width: 125,
 		color: false,
 	    mode: 'hsl',
 	    controls: {
@@ -31,6 +23,14 @@ jQuery(document).ready(function($){
 	    },
 	    border: true, // draw a border around the collection of UI elements
 	    target: false, // a DOM element / jQuery selector that the element will be appended within. Only used when called on an input.
+//		width: 125,
+	};
+ 
+	$('.teccc-color-picker').wpColorPicker(myOptions);
+	
+	// this option doesn't seem to be picked up above
+	$('.teccc-color-picker').iris({
+		width: 125,
 	});
 
 });
