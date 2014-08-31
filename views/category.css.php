a130a53501b28dad5ec87561d072145998fde5c6
Andy Fragen <andy@thefragens.com>
2014-08-30 20:32:16 -0700

    add coloring to responsive month view

--- views/category.css.php
+++ views/category.css.php
@@ -7,6 +7,13 @@
 
 	.tribe-events-list .vevent.hentry h2 { padding-left: 5px; }
 
+	@media only screen and (max-width: 768px) {
+		.tribe-events-calendar td .hentry { display: block; }
+		h3.entry-title.summary,
+		.tribe-events-calendar .tribe-events-has-events:after
+			{ display: none; }
+	}
+
 	<?php Tribe_Events_Category_Colors_Extras::fix_default_week_background(); ?>
 
 	<?php foreach ( $teccc->terms as $id => $attributes ): ?>
