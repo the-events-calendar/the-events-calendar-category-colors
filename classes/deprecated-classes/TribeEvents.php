1dd33057335a249a72ee2dca30c6897bdd974960
Andy Fragen <andy@thefragens.com>
2015-01-25 08:44:49 -0800

    silence deprecated notices

--- classes/deprecated-classes/TribeEvents.php
+++ classes/deprecated-classes/TribeEvents.php
@@ -1,5 +1,5 @@
 <?php
-	_deprecated_file( __FILE__, '3.10', 'Tribe__Events__Events' );
+	//_deprecated_file( __FILE__, '3.10', 'Tribe__Events__Events' );
 
 
 	class TribeEvents extends Tribe__Events__Events {
