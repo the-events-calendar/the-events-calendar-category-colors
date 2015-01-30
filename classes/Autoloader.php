504c25a9efb7df74bc5fc670341cac5918191e26
Andy Fragen <andy@thefragens.com>
2015-01-30 11:02:49 -0800

    fix autoloader for PSR 4 style spacers

--- classes/Autoloader.php
+++ classes/Autoloader.php
@@ -44,10 +44,10 @@ class Autoloader {
 				$class_dir  = dirname( $file );
 				$class_dir  = str_replace( $base, '', $class_dir );
 				$class_dir  = ltrim( $class_dir, '/' );
-				$class_dir  = str_replace( '/', '_', $class_dir );
+				$class_dir  = str_replace( '/', '__', $class_dir );
 				$class_name = str_replace( '.php', '', basename( $file ) );
 				if ( ! empty( $class_dir ) ) {
-					$class_name = $class_dir . '_' . $class_name;
+					$class_name = $class_dir . '__' . $class_name;
 				}
 				$classes[ strtolower( $class_name ) ] = $file;
 			}
