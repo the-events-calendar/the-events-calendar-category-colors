578629483cfc4d81d14874b74ecc7013954762cd
Andy Fragen <andy@thefragens.com>
2015-11-29 16:58:51 -0800

    consistent with TEC

--- src/Back_Compat/Settings_Tab.php
+++ src/Back_Compat/Settings_Tab.php
@@ -1,8 +1,5 @@
 <?php
 
-if ( ! class_exists( 'Tribe__Events__Settings_Tab' ) && class_exists( 'TribeSettingsTab' ) ) {
-	class Tribe__Events__Settings_Tab extends TribeSettingsTab {}
-}
 if ( ! class_exists( 'Tribe__Settings_Tab' ) && class_exists( 'TribeSettingsTab' ) ) {
 	class Tribe__Settings_Tab extends TribeSettingsTab {}
 }
