c4d8434264c1191d553efd73e50f7f5ea5da247a
Andy Fragen <andy@thefragens.com>
2015-11-29 16:50:35 -0800

    add additional deprecated class

--- src/Back_Compat/Settings_Tab.php
+++ src/Back_Compat/Settings_Tab.php
@@ -1,5 +1,8 @@
 <?php
 
+if ( ! class_exists( 'Tribe__Events__Settings_Tab' ) && class_exists( 'TribeSettingsTab' ) ) {
+	class Tribe__Events__Settings_Tab extends TribeSettingsTab {}
+}
 if ( ! class_exists( 'Tribe__Settings_Tab' ) && class_exists( 'TribeSettingsTab' ) ) {
 	class Tribe__Settings_Tab extends TribeSettingsTab {}
 }
