ad569a66a1d3b2e91ac9f767a354678274e5a7a6
Andy Fragen <andy@thefragens.com>
2019-10-26 17:24:27 -0700

    more fixes

--- src/Category_Colors/CSS/Shiny.php
+++ src/Category_Colors/CSS/Shiny.php
@@ -12,7 +12,8 @@ class Shiny {
 		//$css[] = "#tribe-events-content .tribe-events-calendar .tribe-events-category-{$slug} a,";
 		//$css[] = ".type-tribe_events.tribe-events-category-{$slug} h2 a,";
 		//$css[] = ".tribe-events-category-{$slug} > div.hentry.vevent > h3.entry-title a,";
-        //$css[] = ".tribe-events-mobile.tribe-events-category-{$slug} h4 a";
+		//$css[] = ".tribe-events-mobile.tribe-events-category-{$slug} h4 a";
+
         $css[] = "article.tribe_events_cat-{$slug} h3 a,";
 
 		$css[] = '';
@@ -31,7 +32,7 @@ class Shiny {
 		//$css[] = ".tribe-events-category-{$slug} > div.hentry.vevent > h3.entry-title,";
         //$css[] = ".tribe-events-mobile.tribe-events-category-{$slug} h4";
 
-		$css[] = "article.tribe_events_cat-{$slug} h3 a,";
+		$css[] = "article.tribe_events_cat-{$slug} h3,";
 		$css[] = "article.tribe_events_cat-farmers-market a.tribe-events-calendar-month__multiday-event-inner,";
 
 		$css[] = '';
