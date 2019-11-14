ed3553d087f073b9adfb7efdfca067d00d8a927d
Andy Fragen <andy@thefragens.com>
2019-11-13 16:59:13 -0800

    more shiny stuff with pro views,etc

--- src/Category_Colors/CSS/Shiny.php
+++ src/Category_Colors/CSS/Shiny.php
@@ -7,8 +7,8 @@ class Shiny {
 
 		$css[] = "article.tribe_events_cat-{$slug} h3 a,";
 		$css[] = "article.tribe_events_cat-{$slug} h3 a:link,";
-		$css[] = "article.tribe-events-calendar-month__multiday-event.tribe_events_cat-{$slug} h3,";
-		//$css[] = "article.tribe_events_cat-{$slug} .tribe-events-calendar-month__calendar-event-datetime";
+		// $css[] = "article.tribe-events-calendar-month__multiday-event.tribe_events_cat-{$slug} h3,";
+		// $css[] = "article.tribe_events_cat-{$slug} .tribe-events-calendar-month__calendar-event-datetime";
 
 		$css[] = '';
 		$css   = implode( "\n", $css );
@@ -18,14 +18,19 @@ class Shiny {
 	public static function add_background_css( $slug ) {
 		$css = array();
 
-		// $css[] = "article.tribe_events_cat-{$slug} h3,";
 		$css[] = "article.tribe-events-calendar-month__calendar-event.tribe_events_cat-{$slug} h3,";
+		$css[] = "article.tribe-events-calendar-day__event.tribe_events_cat-{$slug} h3,";
+		$css[] = "article.tribe-events-calendar-list__event.tribe_events_cat-{$slug} h3,";
+		$css[] = "article.tribe-events-pro-photo__event.tribe_events_cat-{$slug} h3,";
+		$css[] = "article.tribe-events-pro-map__event-card.tribe_events_cat-{$slug} h3,";
+
+		// $css[] = "article.tribe-events-calendar-day__event.tribe_events_cat-{$slug} header.tribe-events-calendar-day__event-header h3.tribe-events-calendar-day__event-title";
 		// $css[] = "article.tribe-events-calendar-month__multiday-event.tribe_events_cat-{$slug} a.tribe-events-calendar-month__multiday-event-inner,";
 		// $css[] = "article.tribe-events-calendar-month__multiday-event.tribe-events-calendar-month__multiday-event--start.tribe_events_cat-{$slug} a.tribe-events-calendar-month__multiday-event-inner,";
 		$css[] = "article.tribe_events_cat-{$slug} .tribe-events-calendar-month__multiday-event-bar-inner,";
 		// $css[] = "article.tribe_events_cat-{$slug} .tribe-events-calendar-month__calendar-event-details,";
-		$css[] = "article.tribe-events-calendar-month__calendar-event.tribe_events_cat-{$slug} .tribe-events-calendar-month__calendar-event-datetime";
-		$css[] = '.tribe-events article.tribe-events-calendar-month__calendar-event-datetime';
+		// $css[] = "article.tribe-events-calendar-month__calendar-event.tribe_events_cat-{$slug} .tribe-events-calendar-month__calendar-event-datetime,";
+		// $css[] = '.tribe-events article.tribe-events-calendar-month__calendar-event-datetime';
 
 		$css[] = '';
 		$css   = implode( "\n", $css );
