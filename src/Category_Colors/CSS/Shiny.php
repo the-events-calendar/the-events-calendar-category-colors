cb3069518429f351ff8261a9b6efe6e208191060
Andy Fragen <andy@thefragens.com>
2019-11-13 16:23:58 -0800

    more shiny play

--- src/Category_Colors/CSS/Shiny.php
+++ src/Category_Colors/CSS/Shiny.php
@@ -6,7 +6,9 @@ class Shiny {
 		$css = array();
 
 		$css[] = "article.tribe_events_cat-{$slug} h3 a,";
+		$css[] = "article.tribe_events_cat-{$slug} h3 a:link,";
 		$css[] = "article.tribe-events-calendar-month__multiday-event.tribe_events_cat-{$slug} h3,";
+		//$css[] = "article.tribe_events_cat-{$slug} .tribe-events-calendar-month__calendar-event-datetime";
 
 		$css[] = '';
 		$css   = implode( "\n", $css );
@@ -16,10 +18,14 @@ class Shiny {
 	public static function add_background_css( $slug ) {
 		$css = array();
 
-		$css[] = "article.tribe_events_cat-{$slug} h3,";
-		$css[] = "article.tribe-events-calendar-month__multiday-event.tribe_events_cat-{$slug} a.tribe-events-calendar-month__multiday-event-inner,";
+		// $css[] = "article.tribe_events_cat-{$slug} h3,";
+		$css[] = "article.tribe-events-calendar-month__calendar-event.tribe_events_cat-{$slug} h3,";
+		// $css[] = "article.tribe-events-calendar-month__multiday-event.tribe_events_cat-{$slug} a.tribe-events-calendar-month__multiday-event-inner,";
 		// $css[] = "article.tribe-events-calendar-month__multiday-event.tribe-events-calendar-month__multiday-event--start.tribe_events_cat-{$slug} a.tribe-events-calendar-month__multiday-event-inner,";
 		$css[] = "article.tribe_events_cat-{$slug} .tribe-events-calendar-month__multiday-event-bar-inner,";
+		// $css[] = "article.tribe_events_cat-{$slug} .tribe-events-calendar-month__calendar-event-details,";
+		$css[] = "article.tribe-events-calendar-month__calendar-event.tribe_events_cat-{$slug} .tribe-events-calendar-month__calendar-event-datetime";
+		$css[] = '.tribe-events article.tribe-events-calendar-month__calendar-event-datetime';
 
 		$css[] = '';
 		$css   = implode( "\n", $css );
