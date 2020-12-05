95b90b26f595e0c49bca7638d992e27ad77760e1
Andy Fragen <andy@thefragens.com>
2020-12-05 08:14:15 -0800

    composer for master

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -24,24 +24,24 @@ class InstalledVersions
 private static $installed = array (
   'root' => 
   array (
-    'pretty_version' => 'dev-develop',
-    'version' => 'dev-develop',
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
     'aliases' => 
     array (
     ),
-    'reference' => '9a70d43ffe38fa56f9525926831acb7e16fced1a',
+    'reference' => 'db4fcd2dee3da11a04a45ce3ecce1d32edf27fce',
     'name' => 'afragen/the-events-calendar-category-colors',
   ),
   'versions' => 
   array (
     'afragen/the-events-calendar-category-colors' => 
     array (
-      'pretty_version' => 'dev-develop',
-      'version' => 'dev-develop',
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
       'aliases' => 
       array (
       ),
-      'reference' => '9a70d43ffe38fa56f9525926831acb7e16fced1a',
+      'reference' => 'db4fcd2dee3da11a04a45ce3ecce1d32edf27fce',
     ),
   ),
 );
