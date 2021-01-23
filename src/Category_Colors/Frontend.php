6cdd2f65aaefbfcc34324fa69c9e9c01a4f63091
Andy Fragen <andy@thefragens.com>
2021-01-22 21:35:44 -0800

    add filter teccc_uploads_dir
    Fixes https://wordpress.org/support/topic/php-error-related-to-the-temporary-css-file/

--- src/Category_Colors/Frontend.php
+++ src/Category_Colors/Frontend.php
@@ -203,9 +203,17 @@ class Frontend {
 		// Look out for refresh requests.
 		$refresh_css = array_key_exists( 'refresh_css', $_GET );
 		$current     = get_transient( 'teccc_cache_key' );
-		$css_dir     = $this->uploads['basedir'];
-		$css_file    = glob( "{$css_dir}/teccc*.css" );
-		$current     = ! empty( $css_file ) && strpos( $css_file[0], $this->cache_key )
+
+		/**
+		 * Filter the path to `wp-content/uploads` for CSS.
+		 *
+		 * @since 6.4.13
+		 * @param string $this->uploads['basedir'] Path to uploads dir.
+		 */
+		$css_dir  = apply_filters( 'teccc_uploads_dir', $this->uploads['basedir'] );
+		$css_dir  = untrailingslashit( $css_dir );
+		$css_file = glob( "{$css_dir}/teccc*.css" );
+		$current  = ! empty( $css_file ) && strpos( $css_file[0], $this->cache_key )
 			? $current
 			: false;
 
